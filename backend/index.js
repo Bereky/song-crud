c3ca1f4e989c30ff418f3539ebf5ca150ef49b84
unknown <bereketlemma4@gmail.com>
2024-02-07 19:09:49 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8400;
 const router = require("./router/song.routes.js");
 
 // Make connection to database
-connectToDB();
+//connectToDB();
 
 app.use(cors());
 app.use(express.json());
