d49cf6ca132169c94b909f991382d75b9c62235e
unknown <bereketlemma4@gmail.com>
2024-02-08 07:09:13 +0300

    update url

--- frontend/src/app/apis.ts
+++ frontend/src/app/apis.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8400";
+const API_URL = "";
 
 const getSongs = async () => {
   const response = await axios.get(API_URL + "/api/get-song");
