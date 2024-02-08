f25c372f4eedf2369c15f4d3ff51f84612b67e3c
unknown <bereketlemma4@gmail.com>
2024-02-08 06:47:27 +0300

    update mongodb uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_DB_URI_LOCAL, {
+    .connect(process.env.MONGO_DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
